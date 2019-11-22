638b69eecef475219512b66b946f09458dd95ffa
Pierre Gergondet <pierre.gergondet@gmail.com>
2019-11-22 16:59:36 +0800

    [install-dependencies] Implement actions on all platforms

--- install-dependencies/index.js
+++ install-dependencies/index.js
@@ -4,6 +4,48 @@ const io = require('@actions/io');
 const yaml = require('js-yaml');
 const util = require('util');
 
+async function handle_ppa(ppas)
+{
+  ppas.split(' ')
+    .forEach(function(ppa)
+    {
+      await exec.exec('sudo add-apt-repository -y ppa:' + ppa);
+    });
+}
+
+async function build_github_repo(path, ref, btype, options)
+{
+  console.log('--> Cloning ' + path);
+  await exec.exec('git clone --recursive --quiet https://github.com/' + path + ' ' + path)
+  const cwd = process.cwd();
+  io.mkdirP(path + '/build');
+  process.chdir(path + '/build');
+  console.log('--> Configure ' + path);
+  await exec.exec('cmake ../ -DCMAKE_BUILD_TYPE=' + btype + ' ' + options);
+  console.log('--> Building ' + path);
+  await exec.exec('cmake --build . --config ' + btype);
+  console.log('--> Install ' + path);
+  await exec.exec('cmake --build . --target install --config ' + btype);
+}
+
+async function handle_github(github, btype, options)
+{
+  github
+    .forEach(function(entry)
+    {
+      ref = "master";
+      if(entry.ref)
+      {
+        ref = entry.ref;
+      }
+      if(entry.options)
+      {
+        options = options + " " + entry.options;
+      }
+      await build_github_repo(entry.path, ref, btype, options);
+    });
+}
+
 async function run()
 {
   try
@@ -11,17 +53,59 @@ async function run()
     if(process.platform === 'win32')
     {
       const input = yaml.safeLoad(core.getInput('windows'));
-      console.log(JSON.stringify(input, null, 4));
+      if(input.choco)
+      {
+        await exec.exec('choco install ' + input.choco + ' -y');
+      }
+      if(input.pip)
+      {
+        await exec.exec('pip install ' + input.pip);
+      }
+      if(input.github)
+      {
+        const options = '-DCMAKE_INSTALL_PREFIX=C:/devel/install -DBUILD_TESTING:BOOL=OFF';
+        await handle_github(input.github, 'RelWithDebInfo', options);
+      }
     }
     else if(process.platform === 'darwin')
     {
       const input = yaml.safeLoad(core.getInput('macos'));
-      console.log(JSON.stringify(input, null, 4));
+      if(input.brew)
+      {
+        await exec.exec('brew install ' + input.brew);
+      }
+      if(input.pip)
+      {
+        await exec.exec('sudo pip install ' + input.pip);
+        await exec.exec('sudo pip3 install ' + input.pip);
+      }
+      if(input.github)
+      {
+        const options = '-DPYTHON_BINDING_BUILD_PYTHON2_AND_PYTHON3:BOOL=ON -DBUILD_TESTING:BOOL=OFF';
+        await handle_github(input.github, 'RelWithDebInfo', options);
+      }
     }
     else
     {
       const input = yaml.safeLoad(core.getInput('ubuntu'));
-      console.log(JSON.stringify(input, null, 4));
+      if(input.ppa)
+      {
+        await handle_ppa(input.ppa);
+      }
+      if(input.apt)
+      {
+        await exec.exec('sudo apt-get install -y ' + input.apt);
+      }
+      if(input.pip)
+      {
+        await exec.exec('sudo pip install ' + input.pip);
+        await exec.exec('sudo pip3 install ' + input.pip);
+      }
+      if(input.github)
+      {
+        const options = '-DPYTHON_BINDING_BUILD_PYTHON2_AND_PYTHON3:BOOL=ON -DBUILD_TESTING:BOOL=OFF';
+        await handle_github(input.github, 'RelWithDebInfo', options);
+      }
     }
   }
   catch(error)
