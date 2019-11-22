dba674e695e071fe45fd16e3a85796b4ac917618
Pierre Gergondet <pierre.gergondet@gmail.com>
2019-11-22 16:07:22 +0800

    [install-dependencies] Convert input
    Disable get-eigen test for now

--- install-dependencies/index.js
+++ install-dependencies/index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 const io = require('@actions/io');
+const yaml = require('js-yaml');
 const util = require('util');
 
 async function run()
@@ -9,18 +10,18 @@ async function run()
   {
     if(process.platform === 'win32')
     {
-      const input = core.getInput('windows');
-      process.stdout.write(input);
+      const input = yaml.safeLoad(core.getInput('windows'));
+      console.log(JSON.stringify(input, null, 4));
     }
     else if(process.platform === 'darwin')
     {
-      const input = core.getInput('macos');
-      process.stdout.write(input);
+      const input = yaml.safeLoad(core.getInput('macos'));
+      console.log(JSON.stringify(input, null, 4));
     }
     else
     {
-      const input = core.getInput('ubuntu');
-      process.stdout.write(input);
+      const input = yaml.safeLoad(core.getInput('ubuntu'));
+      console.log(JSON.stringify(input, null, 4));
     }
   }
   catch(error)
