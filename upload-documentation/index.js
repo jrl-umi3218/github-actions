04308016cf81d9de6632a7afd7c8d73c549eae02
Pierre Gergondet <pierre.gergondet@gmail.com>
2019-11-29 11:20:58 +0800

    [upload-documentation] Create an action to upload documentation to GitHub

---
+++ upload-documentation/index.js
@@ -0,0 +1,29 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const github = require('@actions/github');
+const io = require('@actions/io');
+
+async function bash(cmd)
+{
+  await exec.exec('bash', ['-c', cmd]);
+}
+
+async function run()
+{
+  try
+  {
+    const GH_USER = core.getInput('GH_USER');
+    const GH_PAGES_TOKEN = core.getInput('GH_PAGES_TOKEN');
+    const GH_REPOSITORY = github.context.repo.owner + '/' + github.context.repo.repo;
+    const cwd = process.cwd();
+    process.chdir(__dirname);
+    await bash('./upload-documentation.sh ' + GH_USER + ' ' + GH_TOKEN + ' ' + GH_REPOSITORY);
+    process.chdir(cwd);
+  }
+  catch(error)
+  {
+    core.setFailed(error.message);
+  }
+}
+
+run();
---
+++ upload-documentation/upload-documentation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+GH_USER=$1
+GH_PAGES_TOKEN=$2
+GH_REPOSITORY=$3
+
+set -x
+git config --global user.name "JRL/IDH Continuous Integration Tool"
+git config --global user.email "jrl-idh+ci@gmail.com"
+cd $GITHUB_WORKSPACE
+git remote set-url origin "https://$GH_USER:$GH_PAGES_TOKEN@github.com/$GH_REPOSITORY"
+if `git fetch --depth=1 origin gh-pages:gh-pages`; then
+  sudo chown -R `whoami` build/
+  cd build/doc && $GITHUB_WORKSPACE/cmake/github/update-doxygen-doc.sh -r $GITHUB_WORKSPACE -b $GITHUB_WORKSPACE/build
+fi
