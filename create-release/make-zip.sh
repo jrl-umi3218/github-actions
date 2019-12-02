f44998e57ad6e30326035f617c743a93965538c1
Pierre Gergondet <pierre.gergondet@gmail.com>
2019-12-02 14:06:43 +0800

    [create-release] Fix zip creation when there is no submodules

--- create-release/make-zip.sh
+++ create-release/make-zip.sh
@@ -16,5 +16,5 @@ cd $REPO_DIR
 PREFIX=$PROJECT_NAME-$TAG
 git archive --verbose --prefix "$PREFIX/" --format "zip" --output "main.zip" HEAD
 git submodule foreach --recursive "git archive --verbose --prefix=\"${PREFIX}/\$path/\" --format zip HEAD --output $REPO_DIR/repo-output-sub-\$sha1.zip"
-zipmerge $OUT main.zip repo-output-*.zip
+zipmerge $OUT main.zip repo-output-*.zip || zipmerge $OUT main.zip
 rm -f main.zip repo-output*.zip
