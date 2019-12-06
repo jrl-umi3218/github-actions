fa9b4e83ff8744ccd8087c5e5f1610df2adfa003
Pierre Gergondet <pierre.gergondet@gmail.com>
2019-12-06 20:02:13 +0800

    [build-package-native] Update before building

--- build-package-native/build-package.sh
+++ build-package-native/build-package.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 @EXTRA_SETUP_COMMANDS@
-mk-build-deps -i -r -t "apt-get -y" /tmp/package/@REPO@/debian/control
+apt-get update -qq
+mk-build-deps -i -r -t "apt-get -y" /package/@REPO@/debian/control
 cd /package/@REPO@
 debuild --no-tgz-check -i -I -b -us -uc
