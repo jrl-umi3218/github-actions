121be55bd159aed0f76d1049aa2750e209d8b3e2
Pierre Gergondet <pierre.gergondet@gmail.com>
2019-12-12 17:56:57 +0800

    [install-dependencies] Export PKG_CONFIG_PATH on Windows

--- install-dependencies/index.js
+++ install-dependencies/index.js
@@ -112,6 +112,11 @@ async function run()
       {
         core.exportVariable('PATH', 'C:\\devel\\install\\bin;' + PATH);
       }
+      PKG_CONFIG_PATH = process.env.PKG_CONFIG_PATH ? process.env.PKG_CONFIG_PATH : "";
+      if(PKG_CONFIG_PATH.indexOf('C:\\devel\\install\\lib\\pkgconfig') == -1)
+      {
+        core.exportVariable('C:\\devel\\install\\lib\\pkgconfig:' + PKG_CONFIG_PATH);
+      }
       const input = yaml.safeLoad(core.getInput('windows'));
       let options = '-DCMAKE_INSTALL_PREFIX=C:/devel/install -DBUILD_TESTING:BOOL=OFF';
       if(input)
