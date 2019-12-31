7246708ae6117b25a91c99c08ccb8d7bb92fbc15
Pierre Gergondet <pierre.gergondet@gmail.com>
2019-12-31 13:19:41 +0800

    [build-package-native] Add date and SHA to the package version

--- build-package-native/build-package.sh
+++ build-package-native/build-package.sh
@@ -4,4 +4,7 @@
 apt-get update -qq
 mk-build-deps -i -r -t "apt-get -y" /package/@REPO@/debian/control
 cd /package/@REPO@
+PKG_SHA=`git rev-parse --short HEAD`
+PKG_DATE=`date +"%Y%m%d%H%M%S"`
+dch -l -$PKG_DATE-$PKG_SHA --distribution `lsb_release -sc` "Automated release"
 debuild --no-tgz-check -i -I -b -us -uc
