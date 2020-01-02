75fb4100d14f713f0efb82c32ab325f1c874ca83
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-01-02 14:38:33 +0800

    [build-package-native] Retry if dependencies installation fails

--- build-package-native/build-package.sh
+++ build-package-native/build-package.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+install-project-dependencies()
+{
+  apt-get update -qq
+  mk-build-deps -i -r -t "apt-get -y" /package/@REPO@/debian/control
+}
+
 @EXTRA_SETUP_COMMANDS@
-apt-get update -qq
-mk-build-deps -i -r -t "apt-get -y" /package/@REPO@/debian/control
+install-project-dependencies
+while [ $? -ne 0 ]
+do
+  echo "Failed to install project dependencies, maybe a mirror sync is running? Trying again in 5 minutes"
+  sleep 5m
+  install-project-dependencies
+done
 cd /package/@REPO@
 PKG_SHA=`git rev-parse --short HEAD`
 PKG_DATE=`date +"%Y%m%d%H%M%S"`
