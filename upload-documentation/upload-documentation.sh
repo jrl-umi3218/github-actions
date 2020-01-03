bbfd3b8789fd9b131a5ed4eac0064af56d83036e
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-01-03 14:26:08 +0800

    [upload-documentation] Copy doc/pictures if it exists

--- upload-documentation/upload-documentation.sh
+++ upload-documentation/upload-documentation.sh
@@ -11,5 +11,9 @@ cd $GITHUB_WORKSPACE
 git remote set-url origin "https://$GH_USER:$GH_PAGES_TOKEN@github.com/$GH_REPOSITORY"
 if `git fetch --depth=1 origin gh-pages:gh-pages`; then
   sudo chown -R `whoami` build/
+  if [ -d doc/pictures ]
+  then
+    cp -r doc/pictures build/doc/doxygen-html/
+  fi
   cd build/doc && $GITHUB_WORKSPACE/cmake/github/update-doxygen-doc.sh -r $GITHUB_WORKSPACE -b $GITHUB_WORKSPACE/build
 fi
