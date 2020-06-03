4ac5b95a7632b1977a93fc857a4542b5fcf6d5f0
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-06-03 14:18:50 +0800

    [build-conan-package] Fix an issue in channel replacement

--- build-conan-package/index.js
+++ build-conan-package/index.js
@@ -106,7 +106,7 @@ async function run()
     {
       package_channel = stable_channel;
     }
-    await bash(`${sed} -i -e's@${repository}/${dev_channel}@${repository}/${package_channel}@' conanfile.py`);
+    await bash(`${sed} -i -e's@${repository}/${stable_channel}@${repository}/${package_channel}@' conanfile.py`);
     await bash(`${sed} -i -e's@${repository}/${dev_channel}@${repository}/${package_channel}@' conanfile.py`);
     await bash('conan info .');
     core.info(`Package channel: ${package_channel}`);
