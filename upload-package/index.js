6daf0faaacb4ccbbf54d853a54355223ef585c2a
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-07-30 17:09:39 +0800

    [upload-package] Retry upload if it failed

--- upload-package/index.js
+++ upload-package/index.js
@@ -75,7 +75,17 @@ async function upload_package(content_api, package, dist, arch, version, deb)
   console.log("Uploading " + deb + " for " + dist + "/" + arch + " (version: " + version + ")");
   const file = fs.readFileSync(deb);
   const path = package.name + '/' + version + '/' + dist + '/' + arch + '/' + deb;
-  await content_api.put(path + ';deb_distribution=' + dist + ';deb_component=main;deb_architecture=' + arch + ';publish=1', file);
+  let retry = 1;
+  const max_retry = 10;
+  while(retry < max_retry)
+  {
+    try {
+      await content_api.put(path + ';deb_distribution=' + dist + ';deb_component=main;deb_architecture=' + arch + ';publish=1', file);
+    } catch(error) {
+      console.log(`Upload failed (try ${retry}/${max_retry}): ${error}`);
+      retry = retry + 1;
+    }
+  }
 }
 
 async function run()
