0bbc4cf6a177fd459db9233c80100fff8fdc4090
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-09-24 19:49:21 +0800

    [install-dependencies] Create the build directory for vcpkg install

--- install-dependencies/index.js
+++ install-dependencies/index.js
@@ -73,6 +73,7 @@ async function handle_vcpkg(vcpkg, compiler)
   core.endGroup();
   core.startGroup('Install dependencies');
     process.chdir(cwd);
+    await io.mkdirP('build');
     await bash(`VCPKG_FEATURE_FLAGS="manifests,binary-caching" ${vcpkg_exe} install --debug --x-install-root=${cwd}/build/vcpkg_installed`);
   core.endGroup();
 }
