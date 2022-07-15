ab5db01bcbb6d9831ff16743ed62b617bee5ef8e
Pierre Gergondet <pierre.gergondet@gmail.com>
2022-07-15 11:36:40 +0200

    Enable TLS1.2 when downloading boost binaries

--- utils/get-boost.ps1
+++ utils/get-boost.ps1
@@ -1,4 +1,5 @@
 # Use the boost_1_77_0-msvc-14.1-64.exe for Windows 2016
 $Url = "https://sourceforge.net/projects/boost/files/boost-binaries/1.77.0/boost_1_77_0-msvc-14.2-64.exe"
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
 (New-Object System.Net.WebClient).DownloadFile($Url, "$env:TEMP\boost.exe")
 Start-Process -Wait -FilePath "$env:TEMP\boost.exe" "/SILENT","/SP-","/SUPPRESSMSGBOXES","/DIR=C:\hostedtoolcache\windows\Boost\1.77.0\x86_64"
