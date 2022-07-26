5b42eff6ec3b860574737cef0c020dc142d49ee3
Pierre Gergondet <pierre.gergondet@gmail.com>
2022-07-26 12:19:18 +0200

    [create-release] Fix tarball generation

--- create-release/make-tar-gz.sh
+++ create-release/make-tar-gz.sh
@@ -12,8 +12,10 @@ PREFIX=$PROJECT_NAME-$TAG
 git archive --verbose --prefix "$PREFIX/" --format "tar" --output "$OUT" HEAD
 git submodule foreach --recursive "git archive --verbose --prefix=\"${PREFIX}/\$path/\" --format tar HEAD --output $REPO_DIR/repo-output-sub-\$sha1.tar"
 if [[ $(ls repo-output-sub*.tar | wc -l) != 0 ]]; then
-  tar --concatenate --file $OUT repo-output-sub*.tar
-  rm -f repo-output-sub*.tar
+  for f in $(ls repo-output-sub*.tar); do
+    tar --concatenate --file $OUT $f
+    rm -f $f
+  done
 fi
 gzip --force --verbose $OUT
 rm -f $OUT
