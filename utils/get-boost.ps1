d0cb583b6d2cf21cc3e3c3ddf60a813d4189ba33
Pierre Gergondet <pierre.gergondet@gmail.com>
2022-10-29 01:24:03 -0700

    [utils] More robust Boost download

--- utils/get-boost.ps1
+++ utils/get-boost.ps1
@@ -1,5 +1,42 @@
 # Use the boost_1_77_0-msvc-14.1-64.exe for Windows 2016
+function DownloadFileWithRetry
+{
+  param(
+    [Parameter(Mandatory = $true)]
+    $Url,
+    [Parameter(Mandatory = $true)]
+    $Out,
+    [ValidateNotNullOrEmpty()]
+    $MaxTry=10
+  )
+  $Try = 1
+  while($Try -le $MaxTry)
+  {
+    if($Try -ne 1)
+    {
+      Write-Host "Download failed, try $Try out of $MaxTry"
+    }
+    try
+    {
+      (New-Object System.Net.WebClient).DownloadFile($Url, $Out)
+      break
+    }
+    catch
+    {
+      if(Test-Path $Out)
+      {
+        Remove-Item $Out
+      }
+      $Try++
+    }
+  }
+  if(Test-Path $Out)
+  {
+    Write-Host "Download succeeded after $Try tries"
+    Get-FileHash $Out | Format-List
+  }
+}
 $Url = "https://sourceforge.net/projects/boost/files/boost-binaries/1.77.0/boost_1_77_0-msvc-14.2-64.exe"
-[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
-(New-Object System.Net.WebClient).DownloadFile($Url, "$env:TEMP\boost.exe")
-Start-Process -Wait -FilePath "$env:TEMP\boost.exe" "/SILENT","/SP-","/SUPPRESSMSGBOXES","/DIR=C:\hostedtoolcache\windows\Boost\1.77.0\x86_64"
+$Out = "$env:TEMP\boost.exe"
+DownloadFileWithRetry -Url "$Url" -Out "$Out"
+Start-Process -Wait -FilePath "$Out" "/SILENT","/SP-","/SUPPRESSMSGBOXES","/DIR=C:\hostedtoolcache\windows\Boost\1.77.0\x86_64"
