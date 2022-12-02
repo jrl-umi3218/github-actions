ca92ccc454891297fa8886fc10eba3fd909e399c
Pierre Gergondet <pierre.gergondet@gmail.com>
2022-12-02 15:00:57 +0900

    [build-package-native] Get the key with curl instead of wget

--- build-package-native/index.js
+++ build-package-native/index.js
@@ -44,7 +44,7 @@ async function run()
     let latest_cmake_cmd = '';
     if(latest_cmake)
     {
-      latest_cmake_cmd = "wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null \\&\\& echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ " + dist + " main' | tee /etc/apt/sources.list.d/kitware.list >/dev/null";
+      latest_cmake_cmd = "curl -1sLf https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null \\&\\& echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ " + dist + " main' | tee /etc/apt/sources.list.d/kitware.list >/dev/null";
     }
 
     commands = [
