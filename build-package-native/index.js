cf2d603354f8e84617f4a81237f223fd20031195
Pierre Gergondet <pierre.gergondet@gmail.com>
2022-12-02 14:34:13 +0900

    [build-package-native] Add an option to use the latest CMake

--- build-package-native/index.js
+++ build-package-native/index.js
@@ -19,6 +19,7 @@ async function run()
     cloudsmith_repo = core.getInput("cloudsmith-repo");
     other_gpg_keys = core.getInput("other-gpg-keys").split(' ').filter(x => x.length != 0);
     other_mirrors = core.getInput("other-mirrors").split(' ').filter(x => x.length != 0);
+    latest_cmake = core.getBooleanInput("latest-cmake");
 
     const context = github.context;
     const repo = context.repo.repo;
@@ -40,8 +41,15 @@ async function run()
       cloudsmith_repo = `curl -1sLf 'https://dl.cloudsmith.io/public/${cloudsmith_repo}/setup.deb.sh' | bash`
     }
 
+    let latest_cmake_cmd = '';
+    if(latest_cmake)
+    {
+      latest_cmake_cmd = "wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null \\&\\& echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ " + dist + " main' | sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null";
+    }
+
     commands = [
       cloudsmith_repo,
+      latest_cmake_cmd,
       other_gpg_keys.map(x => 'apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key ' + x).join(' \\&\\& '),
       other_mirrors.map(x => "echo 'deb " + x + " " + dist + " main' | tee -a /etc/apt/sources.list").join(' \\&\\& ')
     ].filter(x => x.length != 0).join(' \\&\\& ');
