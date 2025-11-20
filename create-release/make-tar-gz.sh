4215eb98bdfb90594ba41662ebceb557b80b228b
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-11-20 21:47:32 +0100

    Revert "fix(packaging): Make sure that submodules are initialized before archiving"
    This reverts commit 0d389442a7533ba75d31f01e2ce337f73d9ca0e0.

--- create-release/make-tar-gz.sh
+++ create-release/make-tar-gz.sh
@@ -9,7 +9,6 @@ OUT=$(pwd)/$PROJECT_NAME.tar
 
 cd $REPO_DIR
 PREFIX=$PROJECT_NAME-$TAG
-git submodule update --init --recursive
 git archive --verbose --prefix "$PREFIX/" --format "tar" --output "$OUT" HEAD
 git submodule foreach --recursive "git archive --verbose --prefix=\"${PREFIX}/\$path/\" --format tar HEAD --output $REPO_DIR/repo-output-sub-\$sha1.tar"
 if [[ $(ls repo-output-sub*.tar | wc -l) != 0 ]]; then
