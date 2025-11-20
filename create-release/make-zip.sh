0d389442a7533ba75d31f01e2ce337f73d9ca0e0
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-11-20 21:32:54 +0100

    fix(packaging): Make sure that submodules are initialized before archiving

--- create-release/make-zip.sh
+++ create-release/make-zip.sh
@@ -13,6 +13,7 @@ OUT=$(pwd)/$PROJECT_NAME.zip
 
 cd $REPO_DIR
 PREFIX=$PROJECT_NAME-$TAG
+git submodule update --init --recursive
 git archive --verbose --prefix "$PREFIX/" --format "zip" --output "main.zip" HEAD
 git submodule foreach --recursive "git archive --verbose --prefix=\"${PREFIX}/\$path/\" --format zip HEAD --output $REPO_DIR/repo-output-sub-\$sha1.zip"
 zipmerge $OUT main.zip repo-output-*.zip || zipmerge $OUT main.zip
